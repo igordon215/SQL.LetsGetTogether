18e4b9561bc6c045104fd9b275559968797b4583
igordon215 <igordon215@gmail.com>
2024-08-11 14:06:11 -0400

    lab done

--- answers/exercise6.sql
+++ answers/exercise6.sql
@@ -1,2 +1,4 @@
 <--List the number of Students in each Country, ordered by the Country with the most Students first,
 <--where the number of students is greater than 10.
+
+SELECT COUNT (Students), Country FROM Students GROUP BY Country ORDER BY COUNT (Students) DESC WHERE Students > 10;
