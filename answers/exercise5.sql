420364f796fb4096a4b7f79f82f34ea84cea05c0
igordon215 <igordon215@gmail.com>
2024-08-11 14:05:43 -0400

    exercise5

--- answers/exercise5.sql
+++ answers/exercise5.sql
@@ -1,2 +1,3 @@
 <--List the number of Students in each Country, ordered by the Country with the most Students first.
 
+SELECT COUNT (Students), Country FROM Students GROUP BY Country ORDER BY COUNT DESC;
